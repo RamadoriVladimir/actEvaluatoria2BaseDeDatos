4c98a2962212ab91f4faeb5efbc450d1f9851229
RamadoriVladimir <vladimirramadoriy@gmail.com>
2024-09-22 20:16:09 -0300

    ahora funciona

--- procesosAlmacenados.py
+++ procesosAlmacenados.py
@@ -4,6 +4,18 @@ class ClienteManager:
     def __init__(self, db_config):
         self.conn = mysql.connector.connect(**db_config)
         self.cursor = self.conn.cursor()
+        self.crear_tabla_si_no_existe()
+
+    def crear_tabla_si_no_existe(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS clientes (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nombre VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL
+        )
+        """
+        self.cursor.execute(query)
+        self.conn.commit()
 
     def add_cliente(self, nombre, email):
         query = "INSERT INTO clientes (nombre, email) VALUES (%s, %s)"
@@ -20,12 +32,14 @@ class ClienteManager:
         self.cursor.close()
         self.conn.close()
 
+
 db_config = {
     'host': 'localhost',
     'user': 'root',
     'password': '',
     'database': 'clientes_db'
 }
+
 
 manager = ClienteManager(db_config)
 manager.add_cliente('Juan Perez', 'juanperez@gmail.com')
